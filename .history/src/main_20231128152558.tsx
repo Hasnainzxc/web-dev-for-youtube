72ff63113288132f101da45e562e005bb0a3824b
hasnainzxc <hasnainzxc@icloud.com>
2023-11-29 15:17:14 +0500

    added pizza app to the yt tuts folder

---
+++ .history/src/main_20231128152558.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+// react v18
+
+function Header() {
+  return <h1> Fast react pizza co </h1>;
+}
+
+function Menu() {}
+
+function Footer() {}
